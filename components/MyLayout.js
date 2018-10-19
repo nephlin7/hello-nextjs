a847e90b80ffb67d6325b7a26249a5dec5640a95
GMartinez <gmartinez@versatecenlinea.net>
2018-10-19 17:03:09 -0600

    upload project

---
+++ components/MyLayout.js
@@ -0,0 +1,19 @@
+import Header from './Header'
+
+
+
+const LayoutStyle =  {
+    margin: 20,
+    padding: 20,
+    border: '1px solid #DDD' 
+}
+
+const MyLayout = ( props ) => (
+
+<div style={LayoutStyle}>
+    <Header />
+    { props.children }
+</div>
+)
+
+export default MyLayout
---
+++ pages/index.js
@@ -0,0 +1,90 @@
+import Layout from '../components/MyLayout'
+import Link from 'next/link'
+// import fetch from 'isomorphic-unfetch'
+
+
+
+
+const PostLink = ({ post }) => (
+    <li>
+      <Link as={`/post/${post.id}`} href={`/post?title=${post.title}`}>
+        <a>{post.title}</a>
+      </Link>
+    </li>
+  )
+    // <Layout>
+    //     <h1>My Blog</h1>
+    //     <ul>
+    //         <PostLink slug="hello-next" title="Hello Next.js"/>
+    //         <PostLink slug="learn-next-js" title="Learn Next.js is awesome"/>
+    //         <PostLink slug="deploy-app-with-Zeit" title="Deploy apps with Zeit"/>
+    //     </ul>
+    // </Layout>
+
+    function getPosts () {
+        return [
+          { id: 'hello-nextjs', title: 'Hello Next.js'},
+          { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+          { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+        ]
+      }
+      
+    const Index = (props) => (
+            <Layout>
+              <h1>My Blog</h1>
+              <ul>
+                {getPosts().map((post) => (
+                  <PostLink key={post.id} post={post}/>
+                ))}
+              </ul>
+              <style jsx>{`
+                h1, a {
+                  font-family: "Arial";
+                }
+          
+                ul {
+                  padding: 0;
+                }
+          
+                li {
+                  list-style: none;
+                  margin: 5px 0;
+                }
+          
+                a {
+                  text-decoration: none;
+                  color: blue;
+                }
+          
+                a:hover {
+                  opacity: 0.6;
+                }
+              `}</style>
+            </Layout>
+          
+        // <Layout>
+        //   <h1>Batman TV Shows</h1>
+        //    <ul>
+        //     {props.shows.map(({show}) => (
+        //       <li key={show.id}>
+        //         <Link as={`/post/${show.id}`} href={`/post?id=${show.id}`}>
+        //           <a>{show.name}</a>
+        //         </Link>
+        //       </li>
+        //     ))}
+        //   </ul> 
+        // </Layout>
+      )
+      
+    //   Index.getInitialProps = async function() {
+    //     const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    //     const data = await res.json()
+      
+    //     console.log(`Show data fetched. Count: ${data.length}`)
+      
+    //     return {
+    //       shows: data
+    //     }
+    //   }
+      
+      export default Index
